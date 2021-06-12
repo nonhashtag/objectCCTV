15862506d251491d0f523598fbe4fd59ca22179f
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2021-06-12 14:38:54 +0900

    Update prototype.py
    원인 모를 성능저하로 인해서 코드를 다시 작성

--- prototype.py
+++ prototype.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from imutils import build_montages
 from datetime import datetime
 import numpy as np
@@ -6,62 +8,64 @@ import argparse
 import imutils
 import cv2
 import time
+
 from twilio.rest import Client
 
 
-#onlist -> [idx, object-N, firsttime, uptime, startX~~~~endY, SMS]
-sms = False
+'''def sns(onlist: list, sent: list):
+    if len(sent)==0:
+        return
+    if any(i in onlist for i in sent):
+        a = sent.index(i)'''
 
+
+
+
+#onlist -> [idx, object-N, firsttime, uptime, startX~~~~endY]l
 def tracker(nextidx, onlist: list, detected: list): #detected-component = [ 0, object-N, confidence, startX, startY, endX, endY ]
     if len(onlist) == 0:
         onlist.append([nextidx[0], detected[1], datetime.now(), datetime.now(), detected[3], detected[4], detected[5], detected[6],
                        False])
         nextidx[0] += 1
-        return
-
-
     for i in range(len(onlist)):
         if (onlist[i][1] == detected[1]
-                and abs(onlist[i][4]-detected[3]) <= 30
-                and abs(onlist[i][5]-detected[4]) <= 30
-                and abs(onlist[i][6]-detected[5]) <= 30
-                and abs(onlist[i][7]-detected[6]) <= 30):
+                and abs(onlist[i][4]-detected[3]) < 40
+                and abs(onlist[i][5]-detected[4]) < 40
+                and abs(onlist[i][6]-detected[5]) < 40
+                and abs(onlist[i][7]-detected[6]) < 40):
             onlist[i][3] = datetime.now()
             onlist[i][4] = detected[3]
             onlist[i][5] = detected[4]
             onlist[i][6] = detected[5]
             onlist[i][7] = detected[6]
             break
-        if (i==len(onlist)-1 and #onlist[i][1] == detected[1]
-            not(abs(onlist[i][4]-detected[3]) <= 30
-               and abs(onlist[i][5]-detected[4]) <= 30
-               and abs(onlist[i][6]-detected[5]) <= 30
-               and abs(onlist[i][7]-detected[6]) <= 30)):
+        if i==len(onlist)-1 and not(onlist[i][1] == detected[1]
+                and abs(onlist[i][4]-detected[3]) < 40
+                and abs(onlist[i][5]-detected[4]) < 40
+                and abs(onlist[i][6]-detected[5]) < 40
+                and abs(onlist[i][7]-detected[6]) < 40):
             onlist.append([nextidx[0], detected[1], datetime.now(), datetime.now(), detected[3], detected[4], detected[5], detected[6],
                            False])
-            print(onlist)
             nextidx[0] += 1
             #onlist.append([idx, detected[1], datetime, i[3], i[4], i[5], i[6], [i[3], i[4], i[5], i[6]]])
             #ㄴ이건 사람객체에도 적용가능하게 할 예정(처음 좌표상태)
-
 
 def timeout(onlist: list):
     to_del = []
     if len(onlist) > 0:
         for i in range(len(onlist)):
-            if (datetime.now() - onlist[i][3]).seconds > 1:
+            if (datetime.now() - onlist[i][3]).seconds > 0.5:
                 to_del.append(i)
-                print(to_del)
+                #print(to_del)
     to_del.reverse()
     if len(to_del) > 0:
         for i in range(len(to_del)):
-            print('시작 {} 끝'.format(to_del[i]))
+            #print('시작 {} 끝'.format(to_del[i]))
             del onlist[to_del[i]]
 
-def indexing(onlist :list, detected: list) -> int:
+def indexing(onlist :list, detected: list) -> str:
     for i in onlist:
-        if (i[1] == detected[1]) and (i[4:8] == detected[3:]):
-            print(type(i[0]))
+        if i[1]==detected[1] and i[4:8]==detected[3:]:
             return str(i[0])
     return "???"
 
@@ -69,11 +73,15 @@ imageHub = imagezmq.ImageHub()
 
 onlist = []
 nextidx = [0]
+
 #클래스 선언
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-"sofa", "train", "tvmonitor"]
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+
+person_N = [0, 15, 100, 0, 0, 0, 0]
+car_N = [0, 7, 100, 0, 0, 0, 0]
 
 #네트워크 불러오기
 net = cv2.dnn.readNetFromCaffe("imagezmq-streaming/MobileNetSSD_deploy.prototxt",
@@ -81,13 +89,12 @@ net = cv2.dnn.readNetFromCaffe("imagezmq-streaming/MobileNetSSD_deploy.prototxt"
 
 #고려하는 객체 지정
 CONSIDER = set(["person", "car", "bus"])
+#CONSIDER = set(["car", "bus"])
 objCount = {obj: 0 for obj in CONSIDER}
 frameDict = {}
 
 lastActive = {}
 lastActiveCheck = datetime.now()
-p_time = datetime.now()
-c_time = datetime.now()
 
 ESTIMATED_NUM_PIS = 4
 ACTIVE_CHECK_PERIOD = 10
@@ -97,133 +104,128 @@ mW = 2
 mH = 2
 
 account_sid = 'API 키'
-auth_token = '권한 토큰'
+auth_token = 'API 토큰'
 client = Client(account_sid, auth_token)
 
 
-record = False
+#video = cv2.VideoCapture("imagezmq-streaming/20210118_172040.mp4")
+#video = cv2.VideoCapture("imagezmq-streaming/20210118_171949.mp4")
+#video = cv2.VideoCapture("imagezmq-streaming/highway.mp4")
+#video = cv2.VideoCapture("cams/20210510_145505.mp4")
+video = cv2.VideoCapture("cams/20210510_143427.mp4") #2번 주정차 1+1
+#video = cv2.VideoCapture(0)
+
+
+
 start_time = time.time()
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
-#video = cv2.VideoCapture(0)
-#video = cv2.VideoCapture("cams/20210510_145505.mp4") #1번 주정차 1대
-video = cv2.VideoCapture("cams/20210510_143427.mp4") #2번 주정차 1+1
-
-
+record = False
 while True:
     rpiName, frame = video.read()
-    ret, frame1 = video.read()
+    #frame = imutils.resize(frame, width=400)
+    frame = imutils.resize(frame, width=640)
+    (h, w) = frame.shape[:2]
+
+    #녹화
     if record == False:
-        #print("녹화 시작")
+        # print("녹화 시작")
         record = True
-        capture = cv2.VideoWriter(time.strftime('%Y-%m-%d %H시 %M분', time.localtime(time.time())) + '.avi', fourcc, 10.0,
-                                (frame1.shape[1], frame1.shape[0]))
+        capture = cv2.VideoWriter('save_video/'+time.strftime('%Y-%m-%d %H시 %M분', time.localtime(time.time())) + '.avi', fourcc, 10.0,
+                                  (frame.shape[1], frame.shape[0]))
     if ((time.time() - start_time) % 60 >= 0 and (time.time() - start_time) % 60 < 0.2):
-        #print("녹화 중지")
+        # print("녹화 중지")
         record = False
         capture.release()
     if record == True:
-        #print("녹화 중..")
-        capture.write(frame1)
+        # print("녹화 중..")
+        capture.write(frame)
 
-    frame = imutils.resize(frame, width=400)
-    (h, w) = frame.shape[:2]
+
+
+
 
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-    0.007843, (300, 300), 127.5)
+                                 0.007843, (300, 300), 127.5)
     timeout(onlist)
-
-# pass the blob through the network and obtain the detections and
-# predictions
+    # pass the blob through the network and obtain the detections and
+    # predictions
     net.setInput(blob)
     detections = net.forward()
 
     objCount = {obj: 0 for obj in CONSIDER}
 
-# loop over the detections
+    # loop over the detections
     for i in np.arange(0, detections.shape[2]):
-    # extract the confidence (i.e., probability) associated with
-    # the prediction
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
         confidence = detections[0, 0, i, 2]
+        #print("{}타입니다!!!".format(detections))
 
         if confidence > 0.2:
             idx = int(detections[0, 0, i, 1])
 
             if CLASSES[idx] in CONSIDER:
                 objCount[CLASSES[idx]] += 1
-
+                #print('시작 {} 끝'.format(CLASSES[idx])) #################################################### 테스트지점
+                #print('시작 {} 끝'.format(detections[0,0,0]))
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
 
-                detected = [0, detections[0, 0, i, 1], datetime, startX, startY, endX, endY]
-                # If it is a Person
-                '''if detections[0,0,i,1]==15:
-                    person_N.append()
-                #If it is a Vehicle
-                elif detections[0,0,i,1] <= 7:
-                    car_N.append()'''
-                # startX, startY, endX, endY = tracker(detections)
+                detected = [0, detections[0,0,i,1], datetime, startX, startY, endX, endY]
+
+                #startX, startY, endX, endY = tracker(detections)
                 tracker(nextidx, onlist, detected)
-                # print('시작 {} {} {} {} 끝'.format(startX, startY, endX, endY))
+                #print('시작 {} {} {} {} 끝'.format(startX, startY, endX, endY))
                 #print(len(onlist))
-                #print(len(onlist))
+                #print(objCount[CLASSES[idx]])
 
                 cv2.rectangle(frame, (startX, startY), (endX, endY),
-                              (255, 0, 0), 2)
+                            (255, 0, 0), 2)
 
-                objidx = CLASSES[idx] + " " + indexing(onlist, detected)
+                objidx=CLASSES[idx] + " " + indexing(onlist, detected)
                 cv2.putText(frame, objidx, (startX, startY - 20), cv2.FONT_ITALIC, 0.5,
-                            (255, 255, 255), 1)
+                                (255, 255, 255), 1)
+                #cv2.putText(frame, rpiName, (startX, startY-20), cv2.FONT_ITALIC, 0.5,
+                #            (255, 255, 255), 1)
+
+    #Show Device ID
+    #cv2.putText(frame, rpiName, (10, 25),
+    #            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    # draw the object count on the frame
+    label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
+                      objCount.items())
+    cv2.putText(frame, label, (10, h - 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
+    #Show Object Class Above the rectangle
+    #if CLASSES[idx] in CONSIDER:
+    #    cv2.putText(frame, label, (startX, startY-20), cv2.FONT_ITALIC, 0.5,
+    #                (255, 255, 255), 1)
 
-#cv2.putText(frame, rpiName, (startX, startY-20), cv2.FONT_ITALIC, 0.5,
-# (255, 255, 255), 1)
-
-#cv2.putText(frame, rpiName, (10, 25),
-# cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-# draw the object count on the frame
-    label = ", ".join("{}: {}".format(obj, count) for (obj, count) in objCount.items())
-    cv2.putText(frame, label, (10, h - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-#Show Object Class Above the rectangle
-    #cv2.putText(frame, label, (startX, startY-20), cv2.FONT_ITALIC, 0.5, (255, 255, 255), 1)
-
-# update the new frame in the frame dictionary
+    # update the new frame in the frame dictionary
     frameDict[rpiName] = frame
 
     montages = build_montages(frameDict.values(), (w, h), (mW, mH))
 
     for (i, montage) in enumerate(montages):
-        cv2.imshow("Monitor ({})".format(i), montage)
-        key = cv2.waitKey(1) & 0xFF
+        cv2.imshow("Home pet location monitor ({})".format(i),
+                   frame)
+                   #montage)
 
-    '''if objCount['person'] != 0:
-        person_t = (datetime.now() - p_time)
-        print("person_t : {}".format(person_t))
-        if person_t.seconds >= 10.0:
-            print("사람 경고")
-            if sms == False :
-                client.api.account.messages.create(to="+수신자", from_="+발신자", body="Person Warn!")
-                sms = True
-    else:
-        p_time = datetime.now()
-        sms = False'''
     if len(onlist) > 0:
         for i in range(len(onlist)):
             if (datetime.now() - onlist[i][2]).seconds >= 10 and onlist[i][1] == 7:
-                print("차량 경고 {}번 차량".format(onlist[i][0]))
                 if onlist[i][8] == False:
                     print("문자")
-                    client.api.account.messages.create(to="+수신자", from_="+발신자", body="Car {} Warn!".format(onlist[i][0]))
+                    print("차량 경고 {}번 차량".format(onlist[i][0]))
+                    #client.api.account.messages.create(to="수신자", from_="발신번호", body="Car {} Warn!".format(onlist[i][0]))
+                    #client.api.account.messages.create(to="수신자", from_="발신번호", body="Car {} Warn!".format(i[0]))
                     onlist[i][8] = True
-            #else:
-                #c_time = datetime.now()
-                #onlist[i][8] = False
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-    if cv2.waitKey(100)>0:
-        break
 
-video.release()
-cv2.destroyAllWindows()
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
